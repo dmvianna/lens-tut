3ffd29d4479d73dd5315620eff9704be5204f0d9
Daniel Vianna <dmlvianna@gmail.com>
2017-12-18 15:33:29 +1100

    molecule

--- src/Main.hs
+++ src/Main.hs
@@ -2,19 +2,34 @@
 
 module Main where
 
-import Control.Lens.Tutorial hiding (Atom, Point, point, x, _element, _point, _x, _y)
+import Control.Lens.Tutorial hiding ( Atom
+                                    , Point
+                                    , Molecule
+                                    , atoms
+                                    , _atoms
+                                    , point
+                                    , x
+                                    , _element
+                                    , _point
+                                    , _x
+                                    , _y)
 import Control.Lens hiding (element)
 
 main = undefined
 
 data Atom = Atom { _element :: String, _point :: Point } deriving (Show)
 data Point = Point { _x :: Double, _y :: Double } deriving (Show)
+data Molecule = Molecule { _atoms :: [Atom] } deriving (Show)
 
 makeLenses ''Atom
 makeLenses ''Point
+makeLenses ''Molecule
 
 -- shiftAtomX :: Atom -> Atom
 -- shiftAtomX (Atom e (Point x y)) = Atom e (Point (x + 1) y)
 
 shiftAtomX :: Atom -> Atom
 shiftAtomX = over (point . x) (+ 1)
+
+shiftMoleculeX :: Molecule -> Molecule
+shiftMoleculeX = over (atoms . traverse . point . x) (+ 1)
