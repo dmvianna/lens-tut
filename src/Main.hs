5b81d849a8735d96e4fdb301c7ab83d6e6203dad
Daniel Vianna <dmlvianna@gmail.com>
2017-12-18 15:24:38 +1100

    atom

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,20 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Main where
 
-import Control.Lens.Tutorial
+import Control.Lens.Tutorial hiding (Atom, Point, point, x, _element, _point, _x, _y)
+import Control.Lens hiding (element)
 
-main :: IO ()
-main = do
-  putStrLn "hello world"
+main = undefined
+
+data Atom = Atom { _element :: String, _point :: Point } deriving (Show)
+data Point = Point { _x :: Double, _y :: Double } deriving (Show)
+
+makeLenses ''Atom
+makeLenses ''Point
+
+-- shiftAtomX :: Atom -> Atom
+-- shiftAtomX (Atom e (Point x y)) = Atom e (Point (x + 1) y)
+
+shiftAtomX :: Atom -> Atom
+shiftAtomX = over (point . x) (+ 1)
